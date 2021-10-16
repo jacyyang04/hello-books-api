4484857551bbc374ef7a1b67021df3bb3c281fb3
jacy <jacyyang04@gmail.com>
2021-10-16 13:39:52 -0700

    add decorator and get_hello_world function

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    #imports hello_world_bp and has blueprint register it as a route
     from .routes import hello_world_bp
     app.register_blueprint(hello_world_bp)
 
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,11 @@
 from flask import Blueprint
 
-hello_world_bp = Blueprint("hello world", __name__)
+hello_world_bp = Blueprint('hello world', __name__)
+
+# using the blueprint decorator: 
+# @@blueprint_name.route("/endpoint/path/here", methods=["GET"])
+@hello_world_bp.route('/hello-world', methods=["GET"])
+def get_hello_world():
+    my_response = "Hello, World!"
+    return my_response
 
