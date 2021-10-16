2df069531b8cc95beb83e8e943fa467916676bd7
jacy <jacyyang04@gmail.com>
2021-10-16 14:14:07 -0700

    add 2 endpoints

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,12 @@ def get_hello_world():
     my_response = "Hello, World!"
     return my_response
 
+@hello_world_bp.route('/hello-world/JSON', methods=["GET"])
+def hello_world_json():
+    return {
+        'name': 'jollyranger',
+        'message': 'Helloooooo!!',
+        'hobbies': ['fishing', 'laughing', 'drinking']
+        
+    }, 201
+
