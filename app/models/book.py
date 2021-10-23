6e65faa0ca8323c531876b99e0e71d19ca9092b7
jacy <jacyyang04@gmail.com>
2021-10-23 10:29:10 -0700

    set up models

---
+++ app/models/book.py
@@ -0,0 +1,7 @@
+from app import db
+
+#an instance of sqlAlchemy
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
