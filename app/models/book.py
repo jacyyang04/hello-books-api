4aa5fa20027ae3f6738e4caee5d5758e2a0d0fa4
jacy <jacyyang04@gmail.com>
2021-11-02 10:04:53 -0700

    establish relationship

--- app/models/book.py
+++ app/models/book.py
@@ -1,7 +1,9 @@
 from app import db
+from sqlalchemy.orm import relationship
 
 #an instance of sqlAlchemy; Book is a child class
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
