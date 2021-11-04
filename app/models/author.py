0a63fdb64dd12b4f67e0688b7c4e68669a44eec8
jacy <jacyyang04@gmail.com>
2021-11-04 14:07:45 -0700

    fixed typo in relationship models

--- app/models/author.py
+++ app/models/author.py
@@ -4,5 +4,5 @@ from app import db
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
-    book = db.relationship('book', backref='author')
+    book = db.relationship('Book', backref='author')
     # books = db.relationship("Book", back_populates="author")
--- app/models/genre.py
+++ app/models/genre.py
@@ -4,5 +4,3 @@ from app import db
 class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
-    # book = db.relationship('book', backref='author')
-    # books = db.relationship("Book", back_populates="author")
