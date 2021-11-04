41bcd49187d9c3aee928e11cde3a4f5098b15fd2
jacy <jacyyang04@gmail.com>
2021-11-04 12:50:11 -0700

    add genre model

---
+++ app/models/genre.py
@@ -0,0 +1,8 @@
+from app import db
+
+# creating one to many relationships
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    # book = db.relationship('book', backref='author')
+    # books = db.relationship("Book", back_populates="author")
